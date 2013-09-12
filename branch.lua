3e5d43e0475eaa79b971002d4d37f45b3579cd60
Jordan Curzon <curzonj@gmail.com>
2013-09-11 23:43:33 -0700

    fix torches

--- branch.lua
+++ branch.lua
@@ -9,7 +9,7 @@ local branch_interval = (prgm_args[2]~=nil and tonumber(prgm_args[2])+1) or 4;
 local branch_length = tonumber(prgm_args[3]) or 50;
 local trunk_length = branches*branch_interval;
 local torch_interval = 7;
-local min_torches = ((branch_length*2) / torch_interval)
+local min_torches = ((branch_length*2 + trunk_length) / torch_interval)
  
 function dumpJunk()
     for x = 1, 3 do
@@ -37,6 +37,9 @@ function dig(length)
   local cLength = 0;
   local bDetect, bMoved;
   
+  -- select the torches
+  turtle.select(16)
+  
   while(cLength < length)do
     repeat turtle.dig(); os.sleep(0.1); bDetect = turtle.detect(); until bDetect == false;
     bMoved = turtle.forward();
@@ -62,9 +65,6 @@ local branch_counter = 0
 while(branch_counter < branches and torch_count > min_torches) do
   branch_counter = branch_counter + 1
 
-  -- select the torches
-  turtle.select(16)
-  
   dig(branch_interval)
   turtle.turnRight()
   dig(branch_length)
