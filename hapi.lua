9dfbb40fea7ce83912a80b2dbdac0204bdd34075
Jordan Curzon <curzonj@gmail.com>
2013-11-10 21:01:04 -0800

    wip

--- hapi.lua
+++ hapi.lua
@@ -1,9 +1,11 @@
 function report(str, tb)
   local url = "http://50.53.175.222:3000/logs?"
 
-  for key, value in pairs(tb) do
-    url_tmp = url .. key .. "=" .. tostring(value) .. "&"
-    url = url_tmp
+  if (tb ~= nil) then
+    for key, value in pairs(tb) do
+      url_tmp = url .. key .. "=" .. tostring(value) .. "&"
+      url = url_tmp
+    end
   end
   
   print(str)
