c822a73ca8c56ff1e63098c4ec393c370b78db60
Jordan Curzon <curzonj@gmail.com>
2014-01-04 10:22:36 -0800

    default_load

---
+++ default_load.lua
@@ -0,0 +1,9 @@
+shell.run("pastebin", "get", "w1RtfqFd", "market")
+shell.run("market", "get", "gjdhav", "at", "y")
+shell.run("market", "get", "gjdhrg", "cube", "y")
+shell.run("market", "get", "gjdh6r", "do", "y")
+
+shell.run("pastebin", "get", "Vqi7FLWB", "git")
+shell.run("git", "collab")
+shell.run("git", "branch")
+shell.run("git", "quarry")
