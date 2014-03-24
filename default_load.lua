ce1be7d6c6d3de93a8458a08d2975b94b6573968
Jordan Curzon <curzonj@gmail.com>
2014-03-24 06:00:07 -0700

    Update default_load.lua
    a better do script

--- default_load.lua
+++ default_load.lua
@@ -3,7 +3,8 @@
 shell.run("pastebin", "get", "w1RtfqFd", "market")
 shell.run("market", "get", "gjdhav", "at", "y")
 shell.run("market", "get", "gjdhrg", "cube", "y")
-shell.run("market", "get", "gjdh6r", "do", "y")
+shell.run("market", "get", "gjdh3r", "act", "y")
+shell.run("market", "get", "gjdh3s", "do", "y")
 
 shell.run("pastebin", "get", "Vqi7FLWB", "git")
 shell.run("git", "collab")
