2e676c1ecb4aa0805c11fdbf5d41fa4e7f08d5ed
Jordan Curzon <curzonj@gmail.com>
2014-04-05 20:32:11 -0700

    Update default_load.lua

--- default_load.lua
+++ default_load.lua
@@ -5,6 +5,11 @@ shell.run("market", "get", "gjdhav", "at", "y")
 shell.run("market", "get", "gjdhrg", "cube", "y")
 shell.run("market", "get", "gjdh3r", "act", "y")
 shell.run("market", "get", "gjdh3s", "do", "y")
+shell.run("market", "get", "gjdh1u", "adt", "y")
+shell.run("market", "get", "gjdh1t", "build", "y")
+
+fs.makeDir("lib")
+fs.move("adt", "/lib/adt") -- this is where build wants it
 
 shell.run("pastebin", "get", "Vqi7FLWB", "git")
 shell.run("git", "collab")
