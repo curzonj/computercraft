d0ccbf1eefdcc3aad08dc10c1833eca86d87df31
Jordan Curzon <curzonj@gmail.com>
2014-05-21 16:44:29 -0700

    fix enderfence

--- enderfence.lua
+++ enderfence.lua
@@ -1,94 +1,47 @@
---Computer Craft script to set up a fence boundary for an ender quarry
---Just run the program and give it the size of the quarry you want
---Author: Vanhal
---http://www.youtube.com/VanhalMinecraft
+local tArgs = { ... }
+local size = tonumber(tArgs[1])
+local action = tArgs[2] or "build"
+local current_slot = 16
 
-local currentSlot = -1
-
-function refuel()
-  if turtle.getFuelLevel() ~= "unlimited" and turtle.getFuelLevel() < 10 then
-    for i = 1, 16 do
-      turtle.select(i)
-      if turtle.refuel(0) then
-        turtle.refuel(1)
-      end
-    end
-  end
+if (size < 1) then
+    error("Usage: enderfence 64 [remove]")
 end
-
-
-function moveForward()
-  refuel()
-  if turtle.detect() then
-    turtle.dig()
-  end
-  turtle.forward()
-end
-
-function placeFence()
-  if turtle.detectUp() then
-    turtle.digUp()
-  end
-  turtle.select(2)
-  if turtle.getItemCount(2) > 0 then
-    if currentSlot > 0 and turtle.compareTo(currentSlot) and turtle.getItemCount(currentSlot) > 0 then
-      turtle.select(currentSlot)
-    else
-      currentSlot = 2
-      for i=3, 16 do
-        if turtle.compareTo(i) then
-          currentSlot = i
-          turtle.select(i)
-        end
-      end
-    end
-    turtle.placeUp()
-  else
-    error("No fences found in slot 2")
-  end
-end
-
---start script and prompt for size
-shell.run('clear')
-print("Please put some fuel in the first slot and put loads stacks of fences in at least the second slot")
-
---just do some error checking
-if turtle.getItemCount(1) == 0 then
-  error("No fuel in slot 1")
-end
-if turtle.getItemCount(2) == 0 then
-  error("No Fences in slot 2")
-end
-turtle.select(1)
-if turtle.refuel(0) == false then
-  error("No fuel in slot 1")
-end
-
-print("How big you want each side of your quarry to be, max 240?")
-local size = io.read()
-
-size = tonumber(size)
 
 function buildRow(rowSize)
   for i=1, rowSize do
-    moveForward()
-    placeFence()
+    if turtle.detect() then
+      turtle.dig()
+    end
+    
+    if not turtle.forward() then
+        error("I'm stuck")
+    end
+    
+    if turtle.detectUp() then
+        turtle.digUp()
+    end
+    
+    if action == "build" then
+    while (turtle.getItemCount(current_slot) == 0) do
+        current_slot = current_slot - 1
+        if (current_slot == 0) then
+            error("The turtle is empty")
+        end
+    end
+    
+    turtle.placeUp()
+    end
   end
 end
 
-if size > 2 then
-  size = size - 1
-  buildRow(size)
-  
-  turtle.turnLeft()
-  buildRow(size)
+size = size - 1
+buildRow(size)
 
-  turtle.turnLeft()
-  buildRow(size)
-  
-  turtle.turnLeft()
-  buildRow(size - 1)
-  moveForward()
-else
-  error("Can't make a square of size 1")
-end
+turtle.turnLeft()
+buildRow(size)
+
+turtle.turnLeft()
+buildRow(size)
+
+turtle.turnLeft()
+buildRow(size - 1)
